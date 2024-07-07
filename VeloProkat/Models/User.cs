21d8ba5bae88cd37aeed6847020fe57bcf478845
NRR <pianzovmaxsim236@gmail.com>
2024-07-07 14:06:43 +0300

    fix

--- VeloProkat/Models/User.cs
+++ VeloProkat/Models/User.cs
@@ -6,13 +6,9 @@ namespace VeloProkat.Models;
 public partial class User
 {
     public int Id { get; set; }
-
     public string Surname { get; set; } = null!;
-
     public string Name { get; set; } = null!;
-
     public string Patronymic { get; set; } = null!;
-
     public string Login { get; set; } = null!;
     public string Password { get; set; } = null!;
     public int Role { get; set; }
