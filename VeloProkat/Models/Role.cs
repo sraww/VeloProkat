b2a7e7cc40822fc4f28da91160c099bcd8a05000
NRR <pianzovmaxsim236@gmail.com>
2024-07-07 14:07:08 +0300

    fix role

--- VeloProkat/Models/Role.cs
+++ VeloProkat/Models/Role.cs
@@ -6,8 +6,6 @@ namespace VeloProkat.Models;
 public partial class Role
 {
     public int Id { get; set; }
-
     public string Name { get; set; } = null!;
-
     public virtual ICollection<User> Users { get; } = new List<User>();
 }
