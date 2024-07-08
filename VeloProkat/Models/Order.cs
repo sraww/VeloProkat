2894fac1f0cb7c73823662729503bbad8d8fced9
NRR <pianzovmaxsim236@gmail.com>
2024-07-08 18:12:52 +0300

    fix

--- VeloProkat/Models/Order.cs
+++ VeloProkat/Models/Order.cs
@@ -8,7 +8,6 @@ public partial class Order
     public int Id { get; set; }
 
     public string Status { get; set; } = null!;
-
     public DateTime DeliveryDate { get; set; }
     public string PickupPoint { get; set; } = null!;
     public virtual ICollection<OrderProduct> OrderProducts { get; } = new List<OrderProduct>();
