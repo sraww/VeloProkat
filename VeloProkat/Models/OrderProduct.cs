14e1722a80a2002681d809a59876a707044efcaf
NRR <pianzovmaxsim236@gmail.com>
2024-07-10 17:44:48 +0300

    fix

--- VeloProkat/Models/OrderProduct.cs
+++ VeloProkat/Models/OrderProduct.cs
@@ -6,12 +6,8 @@ namespace VeloProkat.Models;
 public partial class OrderProduct
 {
     public int Id { get; set; }
-
-    public int OrderId { get; set; }
-
-    public int ProductId { get; set; }
-
-    public virtual Order Order { get; set; } = null!;
-
-    public virtual Product Product { get; set; } = null!;
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public virtual Order Order { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
 }
